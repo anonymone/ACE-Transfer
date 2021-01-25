55fa7c19685aaf3e774c16490efd94e158ae72cb
anonymone <severus.peng@gmail.com>
2021-01-25 22:18:52 +0800

    complete basic ACETransfer logic.

--- Coder/ACETransfer.py
+++ Coder/ACETransfer.py
@@ -6,7 +6,7 @@ from copy import deepcopy
 import uuid
 import logging
 
-from Coder import ACE
+from Coder.ACE import ACE
 
 def build_ACETransfer(fitness_size, ind_params):
     return ACETransfer(fitness_size=fitness_size,
@@ -49,7 +49,7 @@ class ACETransfer(ACE):
             exec("self.{0} = kwargs['{0}']".format(str(name)))
 
     def code_init(self):
-        if codeAssist is not None:
+        if self.code_assist is not None:
             dec = np.array([
                 self.code_assist.getTop(10)[random.randint(0,10)]
                 if random.random() < 0.8
@@ -62,3 +62,72 @@ class ACETransfer(ACE):
             low=self.vb[0], high=self.vb[1]), np.random.randint(low=self.vb[0], high=self.vb[1])] for i in range(random.randint(self.unr[0], self.unr[1]))])
             logging.error("code_assist is None!")
         return dec
+
+class transferNASLearningEngin:
+    def __init__(self):
+        self.knowledgeMap = dict()
+        self.epslion = 0 #init bais
+        self.generalMetric = None
+
+    def parserArchitecture(self, architrecture):
+        componentMap = dict()
+        architrecture = architrecture.replace("<--->", "-").split("-")
+        componentSize = len(architrecture)
+        for component in componentMap:
+            if componentMap not in componentMap:
+                componentMap[component] = 0.0
+            componentMap[component] += 1.0/componentSize
+        return componentMap
+
+    def preLearning(self, knowledgeBase, knowledgeDimension=1): #[architecture:string , parameter:float, error: float,...]
+        if knowledgeBase is None:
+            return False
+        sampleSize = len(knowledgeBase)
+        for i in range(knowledgeDimension):
+            self.knowledgeMap[i] = dict()
+
+        for i in range(sampleSize):
+            sample = knowledgeBase[i]
+            gama = sample[1:]
+            componentMap = self.parserArchitecture(sample[0])
+
+            self.knowledgeMap
+            
+            for component in componentMap:
+                 proportion = componentMap[component]
+                 for metric in self.knowledgeMap:
+                     if component not in self.knowledgeMap[metric].keys():
+                         self.knowledgeMap[metric][component] = [0,0]
+                     self.knowledgeMap[metric][component][0] += gama[metric]*proportion
+        for metric in self.knowledgeMap:
+            self.knowledgeMap[metric]/sampleSize
+        self.calculateGeneralMetric()
+        return True
+
+    def update(self, knowledgeBase):
+        pass
+
+    def getTop(self, number):
+        if self.generalMetric is None:
+            self.calculateGeneralMetric()
+        componentList = list()
+        generalMetricList = list()
+        for component, generalMetric in self.generalMetric:
+            componentList.append(np.array([np.float(element) for element in component]))
+            generalMetricList.append(generalMetric)
+        index = np.argsort(generalMetricList)
+        componentList = componentList[index]
+        return np.array(componentList[:number])
+
+    def calculateGeneralMetric(self):
+        generalMetricDic = dict()
+        componentSize = len(self.knowledgeMap[0])
+
+        for metric in self.componentMap:
+            for component in self.componentMap[metric]:
+                if component not in generalMetricDic:
+                    generalMetricDic[component] = 0.0
+                generalMetricDic[component] += self.componentMap[metric][component]
+        for component in generalMetricDic:
+            self.generalMetric.append((component, generalMetricDic[component]/componentSize))
+        
