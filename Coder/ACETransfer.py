5ac743763a37ce3ca9b3365a63970b197bb792fc
anonymone <severus.peng@gmail.com>
2021-03-27 15:49:24 +0800

    format the code

--- Coder/ACETransfer.py
+++ Coder/ACETransfer.py
@@ -8,17 +8,19 @@ import logging
 
 from Coder.ACE import ACE
 
+
 def build_ACETransfer(fitness_size, ind_params):
     return ACETransfer(fitness_size=fitness_size,
-               unit_number_range=ind_params.unit_num,
-               value_boundary=ind_params.value_boundary,
-               classes=ind_params.classes,
-               layers=ind_params.layers,
-               channels=ind_params.channels,
-               keep_prob=ind_params.keep_prob,
-               drop_path_keep_prob=ind_params.drop_path_keep_prob,
-               use_aux_head=ind_params.use_aux_head,
-               code_assist=ind_params.transfer_nas_learning_engine)
+                       unit_number_range=ind_params.unit_num,
+                       value_boundary=ind_params.value_boundary,
+                       classes=ind_params.classes,
+                       layers=ind_params.layers,
+                       channels=ind_params.channels,
+                       keep_prob=ind_params.keep_prob,
+                       drop_path_keep_prob=ind_params.drop_path_keep_prob,
+                       use_aux_head=ind_params.use_aux_head,
+                       code_assist=ind_params.transfer_nas_learning_engine)
+
 
 class ACETransfer(ACE):
     def __init__(self,
@@ -34,37 +36,38 @@ class ACETransfer(ACE):
                  code_assist=None,
                  **kwargs):
         super(ACETransfer, self).__init__(
-            fitness_size = fitness_size,
-            unit_number_range = unit_number_range,
-            value_boundary = value_boundary,
-            classes = classes,
-            layers = layers,
-            channels = channels,
-            keep_prob = keep_prob,
-            drop_path_keep_prob = drop_path_keep_prob,
-            use_aux_head = use_aux_head,
-            code_assist = code_assist
+            fitness_size=fitness_size,
+            unit_number_range=unit_number_range,
+            value_boundary=value_boundary,
+            classes=classes,
+            layers=layers,
+            channels=channels,
+            keep_prob=keep_prob,
+            drop_path_keep_prob=drop_path_keep_prob,
+            use_aux_head=use_aux_head,
+            code_assist=code_assist
         )
 
     def code_init(self):
         if self.code_assist is not None:
             dec = np.array([
-                self.code_assist.getTop(10)[random.randint(0,9)]
+                self.code_assist.getTop(10)[random.randint(0, 9)]
                 if random.random() < 0.8
                 else [np.random.randint(0, 4) if random.random() < 0.8 else np.random.randint(0, 7), np.random.randint(
-            low=self.vb[0], high=self.vb[1]), np.random.randint(low=self.vb[0], high=self.vb[1])]
-            for _ in range(random.randint(self.unr[0], self.unr[1]))
+                    low=self.vb[0], high=self.vb[1]), np.random.randint(low=self.vb[0], high=self.vb[1])]
+                for _ in range(random.randint(self.unr[0], self.unr[1]))
             ])
         else:
             dec = np.array([[np.random.randint(0, 4) if random.random() < 0.8 else np.random.randint(0, 7), np.random.randint(
-            low=self.vb[0], high=self.vb[1]), np.random.randint(low=self.vb[0], high=self.vb[1])] for i in range(random.randint(self.unr[0], self.unr[1]))])
+                low=self.vb[0], high=self.vb[1]), np.random.randint(low=self.vb[0], high=self.vb[1])] for i in range(random.randint(self.unr[0], self.unr[1]))])
             logging.error("code_assist is None!")
         return dec
+
 
 class transferNASLearningEngin:
     def __init__(self):
         self.knowledgeMap = dict()
-        self.epslion = 0 #init bais
+        self.epslion = 0  # init bais
         self.generalMetric = None
 
     def parserArchitecture(self, architrecture):
@@ -84,12 +87,13 @@ class transferNASLearningEngin:
                 [architecture, parameter, error] = sample.split(",")
                 #architecture = architecture.replace("<--->","-").split("-")
                 #architecture = np.array([np.array([int(i[0]), int(i[1]), int(i[2])]) for i in [unit.split(".") for unit in architecture]])
-                parameter = float(parameter.replace("M",""))
+                parameter = float(parameter.replace("M", ""))
                 error = float(error)
                 knowledgeBase.append((architecture, parameter, error))
         return knowledgeBase
 
-    def preLearning(self, knowledgeBase, knowledgeDimension=1): #[architecture:string , parameter:float, error: float,...]
+    # [architecture:string , parameter:float, error: float,...]
+    def preLearning(self, knowledgeBase, knowledgeDimension=1):
         if knowledgeBase is None:
             return False
         sampleSize = len(knowledgeBase)
@@ -100,13 +104,13 @@ class transferNASLearningEngin:
             sample = knowledgeBase[i]
             gama = sample[1:]
             componentMap = self.parserArchitecture(sample[0])
-            
+
             for component in componentMap:
-                 proportion = componentMap[component]
-                 for metric in self.knowledgeMap:
-                     if component not in self.knowledgeMap[metric].keys():
-                         self.knowledgeMap[metric][component] = [0,0]
-                     self.knowledgeMap[metric][component][0] += gama[metric]*proportion
+                proportion = componentMap[component]
+                for metric in self.knowledgeMap:
+                    if component not in self.knowledgeMap[metric].keys():
+                        self.knowledgeMap[metric][component] = [0, 0]
+                    self.knowledgeMap[metric][component][0] += gama[metric]*proportion
         for metric in self.knowledgeMap:
             for component in self.knowledgeMap[metric]:
                 self.knowledgeMap[metric][component][0]/sampleSize
@@ -117,21 +121,21 @@ class transferNASLearningEngin:
         if knowledgeBase is None:
             return False
         sampleSize = len(knowledgeBase)
-        
+
         for i in range(sampleSize):
             sample = knowledgeBase[i]
             gama = sample[1:]
             componentMap = self.parserArchitecture(sample[0])
-        
+
             for component in componentMap:
                 proportion = componentMap[component]
                 for metric in self.knowledgeMap:
                     if component not in self.knowledgeMap[metric].keys():
-                        self.knowledgeMap[metric][component] = [0,0]
+                        self.knowledgeMap[metric][component] = [0, 0]
                         self.knowledgeMap[metric][component][0] += gama[metric]*proportion
                     else:
-                        self.knowledgeMap[metric][component][1] = 0.9*self.knowledgeMap[metric][component][1] + 
-                                                                  0.1*(gama[metric]*proportion-self.knowledgeMap[metric][component][0])
+                        self.knowledgeMap[metric][component][1] = 0.9*self.knowledgeMap[metric][component][1] + 0.1*(
+                            gama[metric]*proportion-self.knowledgeMap[metric][component][0])
         self.calculateGeneralMetric()
         return True
 
@@ -143,7 +147,8 @@ class transferNASLearningEngin:
         componentList = list()
         generalMetricList = list()
         for component, metric in self.generalMetric:
-            componentList.append(np.array([np.int(element) for element in component.split(".")]))
+            componentList.append(
+                np.array([np.int(element) for element in component.split(".")]))
             generalMetricList.append(metric)
 
         componentList = np.array(componentList)
@@ -166,8 +171,9 @@ class transferNASLearningEngin:
             for component in self.knowledgeMap[metric]:
                 if component not in generalMetricDic:
                     generalMetricDic[component] = 0.0
-                generalMetricDic[component] += sum(self.knowledgeMap[metric][component])
-        
+                generalMetricDic[component] += sum(
+                    self.knowledgeMap[metric][component])
+
         for component in generalMetricDic:
-            self.generalMetric.append((component, generalMetricDic[component]/componentSize))
-        
+            self.generalMetric.append(
+                (component, generalMetricDic[component]/componentSize))
